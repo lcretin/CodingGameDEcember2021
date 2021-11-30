6662eb2a428b01b6b4ca8e1b9ec207b9e2b3152c
lcretin <loic.cretin@amadeus.com>
2021-11-30 22:03:37 +0100

    First Commit into Git to setup project

--- src/com/codinggame/utils/amadeus/Test.java
+++ src/com/codinggame/utils/amadeus/Test.java
@@ -3,6 +3,6 @@ package com.codinggame.utils.amadeus;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello 2");
+        System.out.println("Hello 3");
     }
 }
