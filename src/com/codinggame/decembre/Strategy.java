33445ff59ff3c6f4395bb08865e2db5c6483c8cb
louaque <lpierlot@amadeus.com>
2021-12-02 17:52:41 +0100

    get best bonus

--- src/com/codinggame/decembre/Strategy.java
+++ src/com/codinggame/decembre/Strategy.java
@@ -172,7 +172,7 @@ public class Strategy {
             prefixAllien+= "ALLIEN_ARTIFACT " + tasks[0] + " "+ tasks[1] + " ";
         }
         
-        String colonizeAction="COLONIZE " + distanceToPlay.getStation().getStationId() + " " + distanceToPlay.getPlanet().getPlanetId() + " " + distanceToPlay.getPlanet().getBestBonus();
+        String colonizeAction="COLONIZE " + distanceToPlay.getStation().getStationId() + " " + distanceToPlay.getPlanet().getPlanetId() + " " + distanceToPlay.getPlanet().getBestBonus(myBonus);
         return prefixAllien + colonizeAction;
     }
 }
